664390ea7691dc7a0b3290d94f7019367aa02438
AlexandruScrob <alexader_alexandruscrob@yahoo.com>
2021-09-24 21:45:41 +0300

    added new endpoints + security

--- app.py
+++ app.py
@@ -1,17 +1,47 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 
 app = Flask(__name__)
+app.secret_key = 'alex'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)  # auth endpoint
 
-class Student(Resource):
+items = []
+
+
+class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        return {'student': name}
+        item = filter(lambda x: x['name'] == name, items)
+        first_item = next(item, None)
+
+        return {'item': first_item}, 200 if first_item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {"message": f"An item with name '{name}' already exists "}, 400
+
+        # TODO force=true you don't need the content type header
+        #  silent=True -> doesn't give an error
+        data = request.get_json()
+
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
 
-app.run(port=5000)
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+
+app.run(port=5000, debug=True)
