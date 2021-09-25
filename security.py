e8ef5d81c48eb33c0dd612a6e40f74f703815c06
Alexandru <alexader_alexandruscrob@yahoo.com>
2021-09-25 23:02:46 +0300

    added db

--- security.py
+++ security.py
@@ -1,16 +1,9 @@
 from hmac import compare_digest
 from user import User
 
-users = [
-    User(1, 'bob', 'asdf')
-]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
-
 
 def authenticate(username, password):
-    user = username_mapping.get(username)
+    user = User.find_by_username(username)
 
     if user and compare_digest(user.password, password):
         return user
@@ -18,7 +11,4 @@ def authenticate(username, password):
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id)
-
-
-
+    return User.find_by_id(user_id)
