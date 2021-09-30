34fdfc19c8dbe9abd3e5de9ff0715fd8fa6f9269
Alexandru <alexader_alexandruscrob@yahoo.com>
2021-09-30 22:43:05 +0300

    added propagate exceptions

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from resources.user import UserRegister
 
 
 app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'alex'
