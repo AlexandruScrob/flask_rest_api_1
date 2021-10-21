56e6cf7b7b8d8f2eaa57a37972f08c169c8a80ce
Alexandru <alexader_alexandruscrob@yahoo.com>
2021-10-22 00:44:15 +0300

    image lib, resources, and schemas

--- libs/image_helper.py
+++ libs/image_helper.py
@@ -26,43 +26,62 @@ def get_path(filename: str = None, folder: str = None) -> str:
     return IMAGE_SET.path(filename, folder)
 
 
-def find_image_any_format():
+def find_image_any_format(filename: str, folder: str) -> Union[str, None]:
     """
     Takes a filename and returns an image on any of the accepted formats.
     :return:
     """
-    pass
+    for _format in IMAGES:
+        image = f"{filename}.{_format}"
+        image_path = IMAGE_SET.path(filename=image, folder=folder)
+
+        if os.path.isfile(image_path):
+            return image_path
+
+    return None
 
 
-def _retrieve_filename():
+def _retrieve_filename(file: Union[str, FileStorage]) -> str:
     """
     Take FileStorage and return the file name.
+    Allows our functions to call this with both filenames and
+     FileStorage and always gets back a file name
     :return:
     """
-    pass
+    if isinstance(file, FileStorage):
+        return file.filename
+
+    return file
 
 
-def is_filename_safe():
+def is_filename_safe(file: Union[str, FileStorage]) -> bool:
     """
     Check our regex and return whether the string matches or not.
     :return:
     """
-    pass
+    filename = _retrieve_filename(file)
+
+    allowed_format = "|".join(IMAGES)  # png|svg|jpe|jpg|jpeg
+    regex = f"^[a-zA-Z0-9][a-zA-Z0-9_()-\.]*\.({allowed_format})$"
+
+    return re.match(regex, filename) is not None
 
 
-def get_basename():
+def get_basename(file: Union[str, FileStorage]) -> str:
     """
     Return full name of image in the path
     get_basename('some/folder/image.jpg') returns 'image.jpg'
     :return:
     """
-    pass
+    filename = _retrieve_filename(file)
+    return os.path.split(filename)[1]
 
 
-def get_extension():
+def get_extension(file: Union[str, FileStorage]) -> str:
     """
     Return file extension
     get_basename('image.jpg') returns '.jpg'
     :return:
     """
-    pass
+    filename = _retrieve_filename(file)
+    return os.path.splitext(filename)[1]
